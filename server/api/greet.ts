61e63f747ae8369d97f89ee5cd263b31b4369e09
Dylan Kainth <kainth.dylan@gmail.com>
2024-06-25 08:53:33 +0000

    edited the nuxt.config.js to setup backend

---
+++ server/api/greet.ts
@@ -0,0 +1,11 @@
+// server/api/greet.ts
+
+import { H3Event } from 'h3'
+
+export default defineEventHandler(async (event: H3Event) => {
+    const query = getQuery(event)
+
+    return {
+        greet: `Hello, ${query.name || 'world'}!`,
+    }
+})
