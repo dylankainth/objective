28f59e2535de34047b672437e71c03e73c6f4cff
Dylan Kainth <kainth.dylan@gmail.com>
2024-06-26 09:22:54 +0000

    added db connection to cosmos

--- server/api/makeRequest.ts
+++ server/api/makeRequest.ts
@@ -1,8 +1,31 @@
 // include the required modules
-
 import { BlobServiceClient } from "@azure/storage-blob";
+import { getServerSession } from '#auth'
+import { CosmosClient } from "@azure/cosmos";
 
 export default defineEventHandler(async (event) => {
+
+    const session = await getServerSession(event)
+    if (!session) { return { statusCode: 401, body: JSON.stringify({ message: "Unauthorized" }) };}
+     
+    // connect to cosmosdb using the connection string
+    const client = new CosmosClient(process.env.COSMOSDB_CONNECTION_STRING as string);
+    const database = client.database("production");
+    const container = database.container("bids");
+
+    var item = {
+        'id': '70b63682-b93a-4c77-aad2-65501347265f',
+        'category': 'gear-surf-surfboards',
+        'name': 'Yamba Surfboard',
+        'quantity': 12,
+        'price': 850.00,
+        'clearance': false
+    };
+    
+    var response = await container.items.upsert(item);
+
+    
+
 
     const files = await readMultipartFormData(event);
     files?.forEach((file) => {
--- server/middleware/auth.ts
+++
@@ -1,9 +0,0 @@
-// file: ~/server/middleware/auth.ts
-import { getServerSession } from '#auth'
-
-export default eventHandler(async (event) => {
-  const session = await getServerSession(event)
-  if (!session) {
-    throw createError({ statusMessage: 'Unauthenticated', statusCode: 403 })
-  }
-})
